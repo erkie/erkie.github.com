41cad336a7b5d9b39ea5d6a215278f71b4195e22
Erik Andersson <erik.rothoff@gmail.com>
2011-05-11 07:58:04 +0200

    Added link back to facebook page

--- asteroids.js
+++ asteroids.js
@@ -667,6 +667,12 @@ function Asteroids() {
 		this.points.style.fontWeight = "bold";
 		this.points.className = "ASTEROIDSYEAH";
 		this.navigation.appendChild(this.points);
+		
+		// fb like box
+		this.fbLike = document.createElement('div');
+		this.fbLike.className = 'ASTEROIDSYEAH';
+		this.fbLike.innerHTML = '<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FKick-Ass-Destroy-the-web%2F168200253236727&amp;width=292&amp;colorscheme=light&amp;show_faces=false&amp;stream=false&amp;header=false&amp;height=62" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:330px; height:70px;" allowTransparency="true"></iframe>';
+		this.navigation.appendChild(this.fbLike);
 	} else {
 		this.navigation = document.getElementById('ASTEROIDS-NAVIGATION');
 		this.points = document.getElementById('ASTEROIDS-POINTS');
